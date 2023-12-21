cd26a559c7c6ac5b8217203cc23ee7f898819d54
Abdulmottaleb Elabour <aelabour@outlook.com>
2023-12-21 10:10:21 +0200

    Add project files.

---
+++ functional programming-vs-imperative/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functional_programming_vs_imperative
+{
+    internal class Program
+    {
+        private static List<double> mydata=new List<double>() { 7,4,5,6,3,8,10};
+
+        static void Main(string[] args)
+        {
+
+            #region Imparative 
+             foreach(double x in mydata)
+            {
+                Console.WriteLine(SubtractTen(Square(AddOne(x))).ToString());
+            }
+            Console.Read();
+            #endregion
+
+            //#region Declarative
+
+            //mydata.Select(AddOne).Select(Square).Select(SubtractTen).ToList().ForEach((x)=>Console.WriteLine(x.ToString()));
+
+            //Console.Read();
+            //#endregion
+        }
+
+        public static double AddOne(double x)
+        {
+            return x+1;
+        }
+
+        public static double Square(double x)
+        {
+            return Math.Pow(x,2);
+        }
+
+        public static double SubtractTen(double x)
+        {
+            return x-10;
+        }
+    }
+}
